2451b59053e31e381fbda5a386845ff66f6cef5d
Stefan Kruger <stefan.kruger@uk.ibm.com>
2016-08-05 11:53:59 +0100

    License changed to Apache-2.0. Example file for setting environment vars for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
+#!/usr/bin/env julia
+
 using Couchzilla
 using Base.Test
 
+username = ENV["COUCH_USER"]
+password = ENV["COUCH_PASS"]
+host     = ENV["COUCH_HOST"]
+database = "testdb"
+
+cl = Couchzilla.Client(username, password, "https://$host")
+db, result = Couchzilla.createdb(cl, database=database)
+
 # write your own tests here
 @test 1 == 1
+
