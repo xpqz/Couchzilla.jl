173d9f99dcccd00e3f603d5012acb1d9dbcf5b10
Stefan Kruger <stefan.kruger@uk.ibm.com>
2016-08-25 13:51:33 +0100

    Renaming repo to better conform to Julia standards

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,6 @@ makedocs(modules=[Couchzilla], doctest=true)
  
 deploydocs(
 	deps = Deps.pip("mkdocs"),
-    repo = "github.com/xpqz/couchzilla.git",
+    repo = "github.com/xpqz/Couchzilla.jl.git",
     julia  = "0.4.6",
     osname = "osx")
